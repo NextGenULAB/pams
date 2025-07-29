9d16ba1f1862875df6c304e75128a29f16acf6d2
unknown <sarifulislam536@gmail.com>
2025-07-29 21:23:09 +0600

    Button syntax fixed

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,12 @@ import { Badge } from "@/components/ui/badge";
 export default function Home() {
   return (
     <div className="bg-background"> 
-      <section cclassName="realtive overflow-hidden py-32">
-        <div className="container max-auto px-4">
+      <section className="relative overflow-hidden py-32">
+        <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
            <div>
             <Badge variant="outline"
-            className="bg-emerald-900/30 border-emerald-700/30 px-4 py-2 text-emerald-400 text-sm front-medium"
+            className="bg-emerald-900/30 border-emerald-700/30 px-4 py-2 text-emerald-400 text-sm font-medium"
             >
             Healthcare made simple
             </Badge>
@@ -19,13 +19,32 @@ export default function Home() {
               <span className="gradient-title">anytime, anywhere</span>
               </h1>
               <p className="text-muted-foreground text-lg md:text-xl max-w-md">
-                Book appointments, consult via video, and manage your healthcare journey all un one secure platform.
+                Book appointments, consult via video, and manage your healthcare journey all in one secure platform.
               </p>
-              <div>
-             <Button>Get Started<</Button>
+              <div className="flex flex-col sm:flex-row gap-4">
+             <Button asChild
+             size="lg"
+             className="bg-emerald-600 text-white hover:bg-emerald-700">
+              
+              <Link href={"/onboarding"}>
+              Get Started<ArrowRight className="ml-2 h-4 w-4"/>
+              </Link>
+              </Button>
+              <Button asChild
+             size="lg"
+             className="border-emerald-700/30 hover:bg-muted/80">
+              <Link href={"/doctors"}>Find doctors</Link>
+              </Button>
               </div>
            </div>
-            <div></div>
+            <div className="relative h-[400px] lg:h-[500px] rounded-xl overflow-hidden">
+             <Image src="/banner2.png"
+              alt="Online  Doctors Consultation Platform"
+              fill
+              priority
+              className="object-cover md:pt-14 rounded-xl"
+              />
+            </div>
           </div>  
         </div>
         </section>     

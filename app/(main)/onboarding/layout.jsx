070e1c5faf4a8985772aa34c6201cf36f12693b6
unknown <sarifulislam536@gmail.com>
2025-07-31 21:35:31 +0600

    Implemented welcome section

--- app/(main)/onboarding/layout.jsx
+++ app/(main)/onboarding/layout.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import { getCurrentUser } from "@/actions/onboarding";
 
 export const metadata = {
   title: 'Onboarding - PAMS',
   description: "Complete your profile to get started with PAMS",
 };
 
-const OnboardingLayout = ({ children }) => {
-    return <div>{children}</div>;
+const OnboardingLayout = async ({ children }) => {
+  const user = await getCurrentUser();
+
+  if (user){
+    if(user.role==="PATIENT"){
+      redirect("/doctors");
+    }else if (user.role === "DOCTOR"){
+      if (user.verificationStatus === "VERIFIED"){
+        redirect("/doctor");
+      }else{
+        redirect("/doctor/verification");
+      }
+    }else if (user.role === "ADMIN"){
+      redirect("/admin");
+    }
+  }
+
+    return (
+    <div className='container mx-auto px-4 py-12'>
+      <div className='max-w-3xl mx-auto'>
+       <div className='text-center mb-10'>
+          <h1 className='text -3xl font-bold text-white mb-2 gradient-title'>
+            Welcome to PAMS</h1>
+          <p>Tell us how you want to use the platform</p>
+        </div> 
+        
+        
+        {children}</div>
+      </div>
+    );   
 };
+
 
 export default OnboardingLayout;

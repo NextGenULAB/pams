2f79a38a1679ae43c603373b8a869d81b5717483
unknown <unknownyo66@gmail.com>
2025-07-31 14:42:50 +0600

    onboarding layout initially configured

--- app/(main)/onboarding/layout.jsx
+++ app/(main)/onboarding/layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+export const metadata = {
+  title: 'Onboarding - PAMS',
+  description: "Complete your profile to get started with PAMS",
+};
+
 const OnboardingLayout = () => {
-  return (
-    <div>OnboardingLayout</div>
-  )
-}
+    return <div>OnboardingLayout</div>;
+};
 
 export default OnboardingLayout;

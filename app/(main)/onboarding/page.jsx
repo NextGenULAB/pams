e96a4d5e941e6f863a9b8a8846c9dbcf66df5f9f
Akash10477 <abdullaalbaki10@gmail.com>
2025-08-01 16:08:13 +0600

    implement doctor profile completion page

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -15,6 +15,8 @@ import { toast } from "sonner";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { SPECIALTIES } from "@/lib/specialities";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const doctorFormSchema = z.object({ 
 speciality: z.string().min(1, "speciality is required"),
@@ -71,7 +73,20 @@ const OnboardingPage = () => {
         router.push(data.redirect);
     }
 
- }, [data])
+ }, [data]);
+
+ const OnDoctorSubmit = async (data) => {
+    if (loading) return;
+
+    const formData = new FormData();
+     formData.append("role", "DOCTOR");
+     formData.append("specialty", data.speciality);
+     formData.append("experience", data.experience.toString());
+     formData.append("credentialUrl", data.credentialUrl);
+     formData.append("description", data.description);
+
+     await submitUserRole(formData); 
+ };
  
  if (step === "choose-role"){ 
     return (
@@ -92,7 +107,8 @@ const OnboardingPage = () => {
                 { loading? (
                     <>
                     <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Processing... </>
-                     ) : ("Continue as a Patient") }</Button>
+                     ) : ("Continue as a Patient") }
+                     </Button>
         </CardContent>
     </Card> 
     
@@ -125,19 +141,22 @@ const OnboardingPage = () => {
      className = "border-emerald-900/20">
         
         <CardContent className = "pt-6">
-            <div classname="mb-6">
+            <div className="mb-6">
             
            <CardTitle className= "text-2xl font-bold text-white mb-2">
-            Complite Your Profile
+            Complete Your Profile!
             </CardTitle>
             <CardDescription >
                 Please provide your professional details and verification
             </CardDescription>
             </div>
-            <form className="space-y-6">
+
+            <form className="space-y-6" onSubmit={handleSubmit(OnDoctorSubmit)}>
                 <div className="space-y-2">
             <Label htmlFor="speciality">Medical Speciality</Label>
-            <Select>
+            <Select 
+                value={specialityValue}
+            onValueChange={(value) => setValue("speciality", value)}>
                 <SelectTrigger id="speciality">
                     <SelectValue placeholder="select your speciality" />
                 </SelectTrigger>
@@ -146,18 +165,92 @@ const OnboardingPage = () => {
                     return (<SelectItem
                          key={spec.name}
                         value={spec.name}
-                        className="flex items-center gap-2"
-                    >
+                    ><div className="flex items-center gap-2">
                      <span className="text-emerald-400"> {spec.icon} </span>
                      {spec.name}  
+                     </div>
                     </SelectItem>
                     );
                     })}
-                   
-                   
-                </SelectContent>
+                  </SelectContent>
                 </Select>
+                {errors.speciality && (
+                    <p className="text-sm font-medium text-red-500 mt-1">
+                    {errors.speciality.message}
+                    </p>
+                    )}
                 </div>
+
+            <div className="space-y-2">
+            <Label htmlFor="experience">Years of Experience</Label>
+             <Input 
+                id="experience" 
+                type="number" 
+                placeholder="eg. 5"
+             {...register ("experience", {valueAsNumber: true})}
+             />
+                {errors.experience && (
+                    <p className="text-sm font-medium text-red-500 mt-1">
+                    {errors.experience.message}
+                    </p>
+                    )}
+            </div>
+
+            <div className="space-y-2">
+            <Label htmlFor="credentialUrl">Link to Credential Documents</Label>
+             <Input 
+                id="credentialUrl" 
+                type="url" 
+                placeholder="medical-degree.pdf"
+             {...register ("credentialUrl")}
+             />
+                {errors.credentialUrl && (
+                    <p className="text-sm font-medium text-red-500 mt-1">
+                    {errors.credentialUrl.message}
+                    </p>
+                    )}
+
+                    <p className="text-sm text-muted-foreground">
+                    NB: Please provide a link to your medical degree or certification
+                    </p>
+            </div>
+
+            <div className="space-y-2">
+            <Label htmlFor="description">Description of your services</Label>
+             <Textarea 
+                id="description" 
+                placeholder="Describe your experties, services and approach towards parient care"
+                rows="6"
+             {...register ("description")}
+             />
+                {errors.description && (
+                    <p className="text-sm font-medium text-red-500 mt-1">
+                    {errors.description.message}
+                    </p>
+                    )}
+            </div>
+
+            <div className="pt-2 flex items-center justify-between">
+                <Button
+                    type="button"
+                    variant="outline"
+                    onClick={() => setStep("choose-role")}
+                    className="border-emerald-900/30"
+                     disabled={loading}
+                >
+                    Back</Button>
+
+                <Button type="submit"
+                className="bg-emerald-600 hover:bg-emerald-300"
+                disabled={loading}>
+                   { loading? (
+                    <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> 
+                    Submitting... 
+                    </>
+                     ) : ("Submit for verification") }
+                </Button>
+            </div>
 
             </form>
         </CardContent>
